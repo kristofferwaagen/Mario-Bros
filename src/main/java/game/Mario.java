333a1a60e348625e9a1f9b1971580fb456e843dd
Wågen <kwa045@uib.no>
2022-02-21 05:38:10 +0100

    Hovedklasse for spillet

--- src/main/java/game/Mario.java
+++ src/main/java/game/Mario.java
@@ -5,19 +5,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class Mario extends com.badlogic.gdx.Game {
-    public static final int visionWidth = 400;
-    public static final int visionHeight= 208;
+    public static final int visionWidth = 400; // bredde for spillet
+    public static final int visionHeight= 208; // høyde for spillet
 
-    public SpriteBatch batch;
+    public SpriteBatch batch; // container for forskjellige bilder
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        setScreen(new PlayScreen(this));
+        setScreen(new PlayScreen(this)); // funksjonalitet or å endre aktiv skjerm
     }
 
     @Override
     public void render () {
-        super.render();
+        super.render(); // delegere render metoden til aktiv skjerm (PlayScreen)
     }
 }
