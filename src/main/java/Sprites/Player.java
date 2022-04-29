277074a2e820476d57510b06ca2f4bec1b6f9806
Samuel Berre <qiz015@uib.no>
2022-04-29 12:01:17 +0200

    Bullets følger snur ikke lenger om spiller snur, fiksa spillertestene

--- src/main/java/Sprites/Player.java
+++ src/main/java/Sprites/Player.java
@@ -16,7 +16,7 @@ public class Player extends Sprite {
     public static int hp;
     public Boolean isDead;
     private Boolean removed;
-    public static Boolean flipped = false;
+    public Boolean flipped = false;
     private Boolean testing = true; // Hvis true, kaller musikk, HUD og andre klasser som ikke instansieres i tester.
     public World world;
     public Body b2body;
@@ -66,7 +66,8 @@ public class Player extends Sprite {
             removed = true;
         }
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
-        setRegion((Texture) animation.getKeyFrame(time, true));
+        if(!testing)
+        	setRegion((Texture) animation.getKeyFrame(time, true));
 
         for(Bullets b: bullets){
             b.update(dt);
@@ -134,7 +135,7 @@ public class Player extends Sprite {
 		return 0;
 	}
     public void shootBullets(){
-        bullets.add(new Bullets(screen, b2body.getPosition().x, b2body.getPosition().y));
+        bullets.add(new Bullets(screen, b2body.getPosition().x, b2body.getPosition().y, this));
     }
     public void draw(Batch batch){
         super.draw(batch);
